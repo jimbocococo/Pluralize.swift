f447b8190f2bb447786da49a3c4c41ecb0fb08b9
jimbocococo <79396584+jimbocococo@users.noreply.github.com>
2021-10-26 09:57:21 +1000

    Update Package.swift
    Update MacOS version to v12

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 let package = Package(
     name: "Pluralize.swift",
     platforms: [
-        .macOS(.v11)
+        .macOS(.v12)
         ],
     products: [
         .library(
